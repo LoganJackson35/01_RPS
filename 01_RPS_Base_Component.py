fb7b91f75ae922e201d441ce1d4346c47338e5bb
LoganJackson35 <lj64488@gmail.com>
2021-03-15 20:06:11 +0000

    Finish Intro

---
+++ 01_RPS_Base_Component.py
@@ -0,0 +1,20 @@
+import random
+
+# functions go here
+
+
+# Main routine goes here
+
+# lists of valid responses
+yes_no_list = ["yes", "no"]
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+# ask user if they have played before
+# If 'yes', show instructions
+
+
+# ask users for # of rounds then loop...
+
+
+# Ask user if they want to see their game history
+# if 'yes' show game history
