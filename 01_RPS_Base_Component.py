cf7c32e97f48ee658b1bbc886fd17e1a57be3d58
LoganJackson35 <lj64488@gmail.com>
2021-03-29 20:05:54 +0000

    Game History

--- 01_RPS_Base_Component.py
+++ 01_RPS_Base_Component.py
@@ -42,8 +42,49 @@ def choice_checker(question, valid_list, error_txt):
         print(error_txt)
         print()
 
-# Main routine goes here
+def instructions():
 
+    print("**** How to Play ****")
+    print()
+    print("The rules of the game go here")
+    print()
+    return ""
+
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Please answer yes / no")
+
+
+# Main routine goes here
+played_before = yes_no("Have you played before? ")
+print()
+def statement_generator(statement, decoration):
+
+   sides= decoration * 3
+
+   statement = "{} {} {}".format(sides, statement, sides)
+   top_bottom = decoration * len(statement)
+
+   print(top_bottom)
+   print(statement)
+   print(top_bottom)
+
+   return ""
+
+if played_before == "no":
+    instructions()
 # lists of valid responses
 yes_no_list = ["yes", "no"]
 rps_list = ["rock", "paper", "scissors", "xxx"]
@@ -54,6 +95,12 @@ rps_list = ["rock", "paper", "scissors", "xxx"]
 # ask users for # of rounds then loop...
 rounds_played = 0
 
+# initialise lost / drawn counters
+rounds_lost = 0
+rounds_drawn = 0
+
+# Quick Calculations (stats)
+rounds_won = rounds_played - rounds_lost - rounds_drawn
 
 # Ask user for # of rounds. <enter> for infinite mode
 rounds = check_rounds()
@@ -62,6 +109,8 @@ end_game = "no"
 while end_game == "no":
 
     # Start of Game Play loop
+    if rounds != "" and rounds_played == rounds - 1:
+        end_game = "yes"
 
     # Rounds heading
     print()
@@ -84,29 +133,54 @@ while end_game == "no":
     # ask user for choice and check its valid
     choose = choice_checker(choose_instruction, rps_list,
                             choose_error)
-     # End game if exit code is typed
+    print()
+    print("You chose {}".format(choose))
+    print()
+      # End game if exit code is typed
     if choose == "xxx":
         break
+    else:
+      rounds_played += 1
+
+
+        
     # Get computer choice
 
     comp_choice = random.choice(rps_list[:-1])
     print("Comp Choice: ", comp_choice)
     
     # Compare choices
+    if comp_choice == choose:
+        result = "tie"
+        feedback = "It's a tie"
+        rounds_drawn += 1
+    elif choose == "rock" and comp_choice == "scissors":
+        result = "won"
+        rounds_won += 1
+    elif choose == "paper" and comp_choice == "rock":
+        result = "won"
+        rounds_won += 1
+    elif choose == "scissors" and comp_choice == "paper":
+        result = "won"
+        rounds_won += 1
+    else:
+       result = "lost"
+       rounds_lost += 1
+       
+    if result != "tie":
+      feedback = "{} vs {}  - you {}".format(choose, comp_choice, result)
 
-
-
-
-    # **** rest if loop / game ****
-    elif rounds != "" and rounds_played == rounds - 1:
-            end_game = "yes"
-
-
-    # rest of loop / game
     print()
-    print("You chose {}".format(choose))
-    print()
-    rounds_played += 1
+    print("Won: {}, lost: {}, drawn: {}".format(rounds_won, rounds_lost, rounds_drawn))
 
+    # output results...
+    print(feedback)
+
+
+# end of game statements 
+print()
+print('***** End Game Summary *****')
+print("Won: {} \t|\t Lost: {} \t|\t Draw:"
+"{}".format(rounds_won, rounds_lost, rounds_drawn))
+print()
 print("Thank you for playing")
-                
--- 05_RPS_compare_v1.py
+++ 05_RPS_compare_v1.py
@@ -6,15 +6,32 @@ for item in rps_list:
     for item in rps_list:
       user_choice = rps_list[user_index]
       comp_choice = rps_list[comp_index]
-      user index += 1
+      user_index += 1
 
-      # compare options....
-      if comp_choice == choose:
-        result =  "tie"
+    
+    # Compare choices
+    if comp_choice == user_choice:
+        result = "tie"
+        feedback = "It's a tie"
+        rounds_drawn += 1
+    elif user_choice == "rock" and comp_choice == "scissors":
+        result = "won"
+        rounds_won += 1
+    elif user_choice == "paper" and comp_choice == "rock":
+        result = "won"
+        rounds_won += 1
+    elif user_choice == "scissors" and comp_choice == "paper":
+        result = "won"
+        rounds_won += 1
+    else:
+       result = "lost"
+       rounds_lost += 1
+       
+    if result != "tie":
+      feedback = "{} vs {}  - you {}".format(user_choice, comp_choice, result)
 
+    print()
+    print("Won: {}, lost: {}, drawn: {}".format(rounds_won, rounds_lost, rounds_drawn))
 
-      print("You chose {}, The Computer chose {}.   ")
-            "\nResult: {}".format(user_choice, comp_choice, result))
-
-  comp_index += 1
-  print()
+comp_index += 1
+print()
